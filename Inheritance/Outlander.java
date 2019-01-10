453dc21a1fc5bc1a1037079965cb763bd3f7757a
harunpetekkaya21 <harun.harun007@hotmail.com>
2019-01-10 20:21:30 +0300

    Create Outlander.java

---
+++ Inheritance/Outlander.java
@@ -0,0 +1,32 @@
+package Inheritance.InheritanceChallenge;
+
+
+public class Outlander extends Car {
+    private int roadServiceMonts;
+
+    public Outlander(int roadServiceMonts) {
+        super("Outlander", "4WD",5,5,6,false);
+        this.roadServiceMonts = roadServiceMonts;
+    }
+    public void accelarete(int rate){
+        int newVelocity = getCurrentVelocity()+rate;
+        if (newVelocity==0){
+            stop();
+            changeGear(1);
+        }else if (newVelocity>0 && newVelocity<=10){
+            changeGear(1);
+        }else if (newVelocity>10 && newVelocity<=20){
+            changeGear(2);
+        }else if (newVelocity>20 && newVelocity<=30){
+        changeGear(3);
+        }else{
+            changeGear(4);
+        }
+        if (newVelocity>0){
+            changeVelocity(newVelocity,getCurrentDirection());
+        }
+
+
+    }
+
+}
