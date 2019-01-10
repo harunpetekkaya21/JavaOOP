3328fb79616b9040775d9c94d369c2637fabff58
harunpetekkaya21 <harun.harun007@hotmail.com>
2019-01-10 20:21:48 +0300

    Create Main.java

---
+++ Inheritance/Main.java
@@ -0,0 +1,10 @@
+package Inheritance.InheritanceChallenge;
+
+public class Main {
+    public static void main(String[] args) {
+        Outlander outlander = new Outlander(36);
+        outlander.accelarete(30);
+        outlander.steer(45);
+        outlander.accelarete(90);
+    }
+}
