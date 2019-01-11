ec275dfb5f80450d3b356fc7f952dbe5308c6bae
harunpetekkaya21 <harun.harun007@hotmail.com>
2019-01-11 23:27:56 +0300

    Create Ceiling.java

---
+++ Composition/Ceiling.java
@@ -0,0 +1,17 @@
+public class Ceiling {
+    private int height;
+    private int painttedColor;
+
+    public Ceiling(int height, int painttedColor) {
+        this.height = height;
+        this.painttedColor = painttedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPainttedColor() {
+        return painttedColor;
+    }
+}
