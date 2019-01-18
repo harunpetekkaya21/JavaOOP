ba024c052adaa0eed17b8c4e9cb5b8b002155b14
Harun Petekkaya <harun.harun007@hotmail.com>
2019-01-18 19:29:22 +0300

    Create Resulation.java

---
+++ Composition/App2/Resulation.java
@@ -0,0 +1,19 @@
+package Composite2;
+
+public class Resulation {
+    private int width;
+    private int height;
+
+    public Resulation(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
