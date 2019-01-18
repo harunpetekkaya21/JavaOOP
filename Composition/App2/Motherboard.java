22aff44fed662651d37af81044910d5ae3b8e942
Harun Petekkaya <harun.harun007@hotmail.com>
2019-01-18 19:29:58 +0300

    Create Motherboard.java

---
+++ Composition/App2/Motherboard.java
@@ -0,0 +1,41 @@
+package Composite2;
+
+public class Motherboard {
+    private String model;
+    private String menufacturer;
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+
+    public Motherboard(String model, String menufacturer, int ramSlots, int cardSlots, String bios) {
+        this.model = model;
+        this.menufacturer = menufacturer;
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
+    public void loadProgram(String programName){
+        System.out.println("program " + programName + " is now loading");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMenufacturer() {
+        return menufacturer;
+    }
+
+    public int getRamSlots() {
+        return ramSlots;
+    }
+
+    public int getCardSlots() {
+        return cardSlots;
+    }
+
+    public String getBios() {
+        return bios;
+    }
+}
