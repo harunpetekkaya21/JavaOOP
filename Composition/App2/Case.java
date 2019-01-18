333c7170ae4cfda2694aea8e7989b8b0c2c89fcc
Harun Petekkaya <harun.harun007@hotmail.com>
2019-01-18 19:29:04 +0300

    Create Case.java

---
+++ Composition/App2/Case.java
@@ -0,0 +1,35 @@
+package Composite2;
+
+
+public class Case {
+    private String model;
+    private String menufacturer;
+    private String powerSupply;
+    private Dimensions dimensions;
+
+    public Case(String model, String menufacturer, String powerSupply, Dimensions dimensions) {
+        this.model = model;
+        this.menufacturer = menufacturer;
+        this.powerSupply = powerSupply;
+        this.dimensions = dimensions;
+    }
+    public void pressPoweButton(){
+        System.out.println("Power button pressed");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMenufacturer() {
+        return menufacturer;
+    }
+
+    public String getPowerSupply() {
+        return powerSupply;
+    }
+
+    public Dimensions getDimensions() {
+        return dimensions;
+    }
+}
